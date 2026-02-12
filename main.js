f6f396cac279772b2b005830fecab6c10a27ceb2
Teresa Lew <teresalew92@gmail.com>
2026-02-12 14:24:34 -0800

    feat: implement getHumanChoice function that collects a user's input and logs it to the console

--- main.js
+++ main.js
@@ -4,3 +4,13 @@ function getComputerChoice() {
   const randomIndex = Math.floor(Math.random() * choices.length);
   return choices[randomIndex];
 }
+
+function getHumanChoice() {
+  // get user input
+  let userInput = window.prompt("Enter your choice: rock, paper or scissors");
+  console.log(userInput);
+
+  return userInput;
+}
+
+getHumanChoice();
