dcb473be9ec5d0847dfad91aba1fce3994563a18
Teresa Lew <teresalew92@gmail.com>
2026-02-12 14:55:35 -0800

    feat: implement playRound logic to figure out who wins each round

--- main.js
+++ main.js
@@ -16,4 +16,46 @@ function getHumanChoice() {
   return userInput;
 }
 
-getHumanChoice();
+// paper beats rock
+// scissors beats paper
+// rock beats scissors
+const winsAgainst = {
+  rock: "scissors",
+  paper: "rock",
+  scissors: "paper",
+};
+
+function playRound(humanChoice, computerChoice) {
+  const normalizedHumanChoice = humanChoice.toLowerCase();
+  const normalizedComputerChoice = computerChoice.toLowerCase();
+
+  if (
+    !winsAgainst[normalizedHumanChoice] ||
+    !winsAgainst[normalizedComputerChoice]
+  ) {
+    console.log("Invalid choice.");
+    return;
+  }
+
+  if (winsAgainst[normalizedHumanChoice] === normalizedComputerChoice) {
+    console.log(
+      `You win! ${normalizedHumanChoice} beats ${normalizedComputerChoice}`,
+    );
+    humanScore++;
+  } else if (winsAgainst[normalizedComputerChoice] === normalizedHumanChoice) {
+    console.log(
+      `You lose! ${normalizedComputerChoice} beats ${normalizedHumanChoice}`,
+    );
+    computerScore++;
+  } else {
+    // if equal this is a draw and no one wins
+    console.log("Draw! No one wins.");
+  }
+
+  return;
+}
+
+const computerChoice = getComputerChoice();
+const humanChoice = getHumanChoice();
+
+playRound(humanChoice, computerChoice);
